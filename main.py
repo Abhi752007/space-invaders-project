116c1b88a2109c051dbf6af895ff895ac926e212
Abhinav <abhi752007@gmail.com>
2025-11-23 17:02:39 +0530

    Actual code change

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+
+pygame.init()
+
+running = True
+
+# display
+HEIGHT = 600
+WIDTH = 800
+game_name = "Space Invaders"
+pygame.display.set_caption(game_name)
+#game_icon = pygame.image.load("rocket.png")
+#pygame.display.set_icon(game_icon)
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+
+
+while running:
+
+    screen.fill((0, 0, 0))
+
+    for event in pygame.event.get():
+
+        # To close the window
+        if event.type == pygame.QUIT:
+            running = False
+
