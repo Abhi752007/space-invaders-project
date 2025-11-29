7fa6ba09a3fffacc92b5e0fd6f1ce788c7aaa8f7
Naonith <naonithkrao11@gmail.com>
2025-11-29 19:10:34 +0530

    enemy code

--- enemy/enemy.py
+++ enemy/enemy.py
@@ -1,15 +1,45 @@
 import pygame
+import player
+import random
 
-print("Pygame imported successfully.")
-print("pygame")
+#storing all the enemies in lists 
+enemy_image = []
+enemy_X = []
+enemy_Y = []
+enemy_x_change = []
+enemy_Y_change = []
+num_of_enemy = 12
 
-print("enem")
-print("trial")
-print("trial2")
-print("trial 3")
+# spawning the enemies
+for i in range(num_of_enemy):
+    enemy_image.append(pygame.image.load('space-invaders.png'))
+    enemy_X.append(random.randint(0, 400))
+    enemy_Y.append(random.randint(50, 150))
+    enemy_x_change.append(0.3)
+    enemy_Y_change.append(0.2)
+
+def player(x,y):
+    screen.blit(Playerimage, (x, y))
+
+def enemy(x,y):
+    screen.blit(enemy_image, (x, y))
 
 
-
-
-
-
+# Movement of  enemies
+for i in range(num_of_enemy):
+    enemy_X[i] += enemy_x_change[i]
+    if enemy_X[i] <= 0:
+        enemy_X_change[i] = 4 
+        enemy_Y[i] += enemy_Y_change[i]
+    elif enemy_X[i] >= 736:
+        enemy_X_change[i] = -4 
+        enemy_Y[i] += enemy_Y_change[i]
+    
+    collsion = iscollsion(enemy_X[i], enemy_Y[i], bullet_X, bullet_Y)
+    if collsison:
+        bullet_Y = 480
+        bullet_state = "ready"
+        score += 1
+        print(score)
+        enemy_X[i] = random.randint(0,800)
+        enemy_Y[i] = random.randint(50,150)
